01fcef7429213e08b2af0215ab5c73ec3255b8a4
David Higgins <thehig@gmail.com>
2018-02-19 18:00:26 +0000

    Create FormControl for Navigation

--- src/atomic/organisms/Navigation.jsx
+++ src/atomic/organisms/Navigation.jsx
@@ -2,27 +2,77 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
+import Radio, { RadioGroup } from 'material-ui/Radio';
+import {
+  FormLabel,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+} from 'material-ui/Form';
+import TextField from 'material-ui/TextField';
 
 import { withStyles } from 'material-ui/styles';
 const styles = theme => ({
   root: {
-    width: "100%"
-  }
+    display: 'flex',
+  },
+  formControl: {
+    margin: theme.spacing.unit * 3,
+  },
+  group: {
+    margin: `${theme.spacing.unit}px 0`,
+  },
 });
 
-const Navigation = ({classes}) => (
-  <Paper className={classes.root}>
-    <Grid container className={classes.root}>
-      <Grid item xs={6}>
-        <Typography variant="subheading">Navigation</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <div>
-          DISADV, PROF, ADV
-        </div>
-      </Grid>
-    </Grid>
-  </Paper>
-);
+class Navigation extends React.Component {
+  state = {
+    value: 'normal',
+    bonus: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleNameChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <FormControl
+        component="fieldset"
+        className={classes.formControl}
+      >
+        <FormLabel component="legend">Navigation</FormLabel>
+        <RadioGroup
+          aria-label="navigation"
+          name="navigation"
+          className={classes.group}
+          value={this.state.value}
+          onChange={this.handleChange}
+          row
+        >
+          <FormControlLabel value="disadv" control={<Radio />} label="Disadvantage" />
+          <FormControlLabel value="normal" control={<Radio />} label="Normal" />
+          <FormControlLabel value="adv" control={<Radio />} label="Advantage" />
+        </RadioGroup>
+        <TextField
+          id="bonus"
+          label="Bonus"
+          className={classes.textField}
+          value={this.state.bonus}
+          onChange={this.handleNameChange('name')}
+          type="number"
+          margin="normal"
+        />
+      </FormControl>
+    );
+  }
+}
 
 export default withStyles(styles)(Navigation);
