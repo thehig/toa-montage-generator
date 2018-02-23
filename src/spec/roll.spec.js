5a7fca87d65fbb3f8b5094d0d041d09150c082dd
David Higgins <thehig@gmail.com>
2018-02-23 17:32:35 +0000

    Correct bracketing typo

--- src/spec/roll.spec.js
+++ src/spec/roll.spec.js
@@ -16,17 +16,17 @@ describe('roll', () => {
   it('rolls without adv or disadv', () => expect(roll(d20RollsOne)).toBe(1));
 
   it('rolls with adv', () => {
-    expect(roll(d20RollsHigher, { advantage: true })).toBe(2));
-    expect(roll(d20RollsLower, { advantage: true })).toBe(20));
+    expect(roll(d20RollsHigher, { advantage: true })).toBe(2);
+    expect(roll(d20RollsLower, { advantage: true })).toBe(20);
   });
 
   it('rolls with disadv', () => {
-    expect(roll(d20RollsHigher, { disadvantage: true })).toBe(1));
+    expect(roll(d20RollsHigher, { disadvantage: true })).toBe(1);
     expect(roll(d20RollsLower, { disadvantage: true })).toBe(19);
   });
 
   it('rolls with both', () => {
-    expect(roll(d20RollsHigher, { advantage: true, disadvantage: true })).toBe(1));
-    expect(roll(d20RollsLower, { advantage: true, disadvantage: true })).toBe(20));
+    expect(roll(d20RollsHigher, { advantage: true, disadvantage: true })).toBe(1);
+    expect(roll(d20RollsLower, { advantage: true, disadvantage: true })).toBe(20);
   });
 });
