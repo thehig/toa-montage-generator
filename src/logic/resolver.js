22b9de4bb2ffe433065d57538497a0feb50e3c6f
David Higgins <thehig@gmail.com>
2018-02-25 18:16:21 +0000

    Retest navigation check

--- src/logic/resolver.js
+++ src/logic/resolver.js
@@ -1,158 +1,291 @@
-import { dice as defaultDice, roll as defaultRoll } from './dice';
-import {
-  directions as defaultDirections,
-  speeds as defaultSpeeds,
-  paceModifiers as defaultPaceModifiers,
-} from '../data/consts';
+export const resolver = ({ dice }) => {
+  const resolverFunctions = {};
 
-// const navigationCheck =  ({ DC = 15, pace = 'normal' } = {}) => roll(d20, { advantage, disadvantage }) + modifier >= DC + myPaces(pace).paceDC;
+  resolverFunctions.navigationCheck = ({ navigator, DC = 10, paceMod = 0 }) =>
+    dice.d20({
+      advantage: navigator.advantage,
+      disadvantage: navigator.disadvantage,
+      modifier: navigator.modifier,
+      versus: DC + paceMod,
+    });
 
-// const paces = ({
-//   dice = dice,
-//   paceModifiers = paceModifiers,
-//   speeds = speeds
-// } = {}) => ({pace = "normal", speed="default"}) => {
+  return resolverFunctions;
+};
+
+// const daysTravel = ({ dice }) =>
+
+// import { dice as defaultDice, roll as defaultRoll } from './dice';
+
+// import {
+
+//   directions as defaultDirections,
+
+//   speeds as defaultSpeeds,
+
+//   paceModifiers as defaultPaceModifiers,
+
+// } from '../data/consts';
+
+// // const navigationCheck =  ({ DC = 15, pace = 'normal' } = {}) => roll(d20, { advantage, disadvantage }) + modifier >= DC + myPaces(pace).paceDC;
+
+// // const paces = ({
+
+// //   dice = dice,
+
+// //   paceModifiers = paceModifiers,
+
+// //   speeds = speeds
+
+// // } = {}) => ({pace = "normal", speed="default"}) => {
+
+// //   switch (pace) {
+
+// //     case 'slow':
+
+// //       return {
+
+// //         paceDC: paceModifiers[pace],
+
+// //         distance: speeds[speed] - (dice.d4() <= 2 && 1) };
+
+// //     case 'fast':
+
+// //       return { paceDC: +5, distance: speeds[speed] + (dice.d4() > 2 && 1) };
+
+// //     default:
+
+// //       return { paceDC: 0, distance: speed };
+
+// //   }
+
+// // };
+
+// const navigationCheckBase = ({
+
+//   dice = defaultDice,
+
+//   roll = defaultRoll,
+
+//   paceModifiers = defaultPaceModifiers,
+
+//   terrain: { id = 0, name = 'DEFAULT_TERRAIN', navDC = 10, encChance = 20 } = {
+
+//   },
+
+// }) => (
+
+//   {
+
+//     // Navigator
+
+//     modifier = 0,
+
+//     pace = 'normal',
+
+//     advantage = false,
+
+//     disadvantage = false,
+
+//   } = {}
+
+// ) => () =>
+
+//   roll(dice.d20, { advantage, disadvantage })(`nav check${advantage ? ' ADV' : ''}${disadvantage ? ' DISADV' : ''}`) + modifier >=
+
+//   navDC + (paceModifiers[pace] || 0);
+
+// export const resolver = (
+
+//   {
+
+//     // World
+
+//     dice = defaultDice,
+
+//     roll = defaultRoll,
+
+//     directions = defaultDirections,
+
+//     speeds = defaultSpeeds,
+
+//     paceModifiers = defaultPaceModifiers,
+
+//     terrain,
+
+//   } = {}
+
+// ) => (
+
+//   {
+
+//     // Navigator
+
+//     survivalMod = 0,
+
+//     miscMod = 0,
+
+//     advantage = false,
+
+//     disadvantage = false,
+
+//     speed = 'default',
+
+//     pace = 'normal',
+
+//   } = {}
+
+// ) => {
+
+//   const navigationCheck = navigationCheckBase({
+
+//     dice,
+
+//     roll,
+
+//     paceModifiers,
+
+//     terrain,
+
+//   })({
+
+//     modifier: survivalMod + miscMod,
+
+//     pace,
+
+//     advantage,
+
+//     disadvantage,
+
+//   });
+
+//   return {
+
+//     navigationCheck,
+
+//   };
+
+// };
+
+// /*
+
+// // const myPaces = paces({ d4 });
+
+// // return {
+
+// //   navigation: ({ DC = 15, pace = 'normal' } = {}) =>
+
+// //     roll(d20, { advantage, disadvantage }) + modifier >=
+
+// //     DC + myPaces(pace).paceDC,
+
+// //   direction: () => directions[d6() - 1],
+
+// //   distance: ({ pace, speed } = {}) => myPaces(pace, speed).distance,
+
+// //   encounter: encounterDC => d20() >= encounterDC && d100(),
+
+// // };
+
+// export const paces = ({ d4 }) => (pace, speed = 1) => {
+
 //   switch (pace) {
+
 //     case 'slow':
-//       return {
-//         paceDC: paceModifiers[pace],
-//         distance: speeds[speed] - (dice.d4() <= 2 && 1) };
+
+//       return { paceDC: -5, distance: speed - (d4() <= 2 && 1) };
 
 //     case 'fast':
-//       return { paceDC: +5, distance: speeds[speed] + (dice.d4() > 2 && 1) };
+
+//       return { paceDC: +5, distance: speed + (d4() > 2 && 1) };
 
 //     default:
+
 //       return { paceDC: 0, distance: speed };
+
 //   }
+
 // };
 
-const navigationCheckBase = ({
-  dice = defaultDice,
-  roll = defaultRoll,
-  paceModifiers = defaultPaceModifiers,
-  terrain: { id = 0, name = 'DEFAULT_TERRAIN', navDC = 10, encChance = 20 } = {
-  },
-}) => (
-  {
-    // Navigator
-    modifier = 0,
-    pace = 'normal',
-    advantage = false,
-    disadvantage = false,
-  } = {}
-) => () =>
-  roll(dice.d20, { advantage, disadvantage })(`nav check${advantage ? ' ADV' : ''}${disadvantage ? ' DISADV' : ''}`) + modifier >=
-  navDC + (paceModifiers[pace] || 0);
-export const resolver = (
-  {
-    // World
-    dice = defaultDice,
-    roll = defaultRoll,
-    directions = defaultDirections,
-    speeds = defaultSpeeds,
-    paceModifiers = defaultPaceModifiers,
-    terrain,
-  } = {}
-) => (
-  {
-    // Navigator
-    survivalMod = 0,
-    miscMod = 0,
-    advantage = false,
-    disadvantage = false,
-    speed = 'default',
-    pace = 'normal',
-  } = {}
-) => {
-  const navigationCheck = navigationCheckBase({
-    dice,
-    roll,
-    paceModifiers,
-    terrain,
-  })({
-    modifier: survivalMod + miscMod,
-    pace,
-    advantage,
-    disadvantage,
-  });
+// export const resolver = ({ d2, d4, d6, d8, d10, d12, d20, d100 }) => (
 
-  return {
-    navigationCheck,
-  };
-};
+//   { modifier = 0, advantage = false, disadvantage = false } = {}
 
-/* 
-// const myPaces = paces({ d4 });
-// return {
-//   navigation: ({ DC = 15, pace = 'normal' } = {}) =>
-//     roll(d20, { advantage, disadvantage }) + modifier >=
-//     DC + myPaces(pace).paceDC,
-//   direction: () => directions[d6() - 1],
-//   distance: ({ pace, speed } = {}) => myPaces(pace, speed).distance,
-//   encounter: encounterDC => d20() >= encounterDC && d100(),
+// ) => {
+
 // };
 
-export const paces = ({ d4 }) => (pace, speed = 1) => {
-  switch (pace) {
-    case 'slow':
-      return { paceDC: -5, distance: speed - (d4() <= 2 && 1) };
+// export const montage = resolver => (
 
-    case 'fast':
-      return { paceDC: +5, distance: speed + (d4() > 2 && 1) };
+//   {
 
-    default:
-      return { paceDC: 0, distance: speed };
-  }
-};
+//     terrain = {},
 
-export const resolver = ({ d2, d4, d6, d8, d10, d12, d20, d100 }) => (
-  { modifier = 0, advantage = false, disadvantage = false } = {}
-) => {
+//     navigator = {},
 
-};
+//     numberOfDays = 10,
 
-export const montage = resolver => (
-  {
-    terrain = {},
-    navigator = {},
-    numberOfDays = 10,
-    pace = 'normal',
-    speed = 1,
-  } = {}
-) => {
-  const myResolver = resolver(navigator);
+//     pace = 'normal',
 
-  const daysResults = [];
-  let lost = false;
+//     speed = 1,
 
-  for (let i = 0; i < numberOfDays; i++) {
-    const day = {
-      day: i,
-      encounters: [
-        myResolver.encounter(terrain.encounterDC),
-        myResolver.encounter(terrain.encounterDC),
-        myResolver.encounter(terrain.encounterDC),
-      ],
-      distance: myResolver.distance({ speed, pace }),
-    };
+//   } = {}
 
-    const navCheck = myResolver.navigation({ DC: terrain.DC, pace });
+// ) => {
 
-    if (!navCheck) {
-      lost = true;
-      day.lost = true;
-      day.direction = myResolver.direction();
-    }
-    
-    daysResults.push(day);
+//   const myResolver = resolver(navigator);
 
-    // Stop on encounter
-    if(day.encounters.filter(enc => enc !== false).length > 0) break;
-    // Stop on found
-    if(navCheck && lost) break;
-  }
+//   const daysResults = [];
 
-  return daysResults;
-};
-export default resolver;
- */
+//   let lost = false;
+
+//   for (let i = 0; i < numberOfDays; i++) {
+
+//     const day = {
+
+//       day: i,
+
+//       encounters: [
+
+//         myResolver.encounter(terrain.encounterDC),
+
+//         myResolver.encounter(terrain.encounterDC),
+
+//         myResolver.encounter(terrain.encounterDC),
+
+//       ],
+
+//       distance: myResolver.distance({ speed, pace }),
+
+//     };
+
+//     const navCheck = myResolver.navigation({ DC: terrain.DC, pace });
+
+//     if (!navCheck) {
+
+//       lost = true;
+
+//       day.lost = true;
+
+//       day.direction = myResolver.direction();
+
+//     }
+
+//     daysResults.push(day);
+
+//     // Stop on encounter
+
+//     if(day.encounters.filter(enc => enc !== false).length > 0) break;
+
+//     // Stop on found
+
+//     if(navCheck && lost) break;
+
+//   }
+
+//   return daysResults;
+
+// };
+
+// export default resolver;
+
+//  */
---
+++ src/spec/resolver.spec.js
@@ -0,0 +1,50 @@
+import { _dArray } from '../logic/dice';
+import { resolver } from '../logic/resolver';
+
+describe('resolver', () => {
+  it('takes world params and returns an object', () =>
+    expect(typeof resolver({ some: 'function' })).toBe('object'));
+
+  describe('navigation check', () => {
+    it('takes navigator, terrain and pace params', () => {
+      const navcheck = resolver({ dice: { d20: _dArray([11]) } }).navigationCheck;
+      const navCheck = navcheck({
+        navigator: {},
+        DC: 10,
+        paceMod: 5,
+      });
+      expect(typeof navCheck).toBe('object');
+    });
+
+    const navcheck = resolver({ dice: { d20: _dArray([11]) } }).navigationCheck;
+
+    describe('with a bad navigator (-1)', () => {
+      const navigator = { modifier: -1 };
+
+      it('fails with a roll of 11 vs 15', () =>
+        expect(navcheck({ navigator, DC: 15 }).success).toBe(false));
+
+      it('fails with a roll of 11 vs 11', () =>
+        expect(navcheck({ navigator, DC: 11 }).success).toBe(false));
+
+      it('succeeds with a roll of 11 vs 10', () =>
+        expect(navcheck({ navigator, DC: 10 }).success).toBe(true));
+
+      it('succeeds with a roll of 11 vs 7', () =>
+        expect(navcheck({ navigator, DC: 7 }).success).toBe(true));
+    });
+
+    describe('with a good navigator (+1)', () => {
+      const navigator = { modifier: +1 };
+
+      it('fails with a roll of 11 vs 15', () => expect(navcheck({ navigator, DC: 15 }).success).toBe(false));
+
+      it('fails with a roll of 11 vs 13', () => expect(navcheck({ navigator, DC: 13 }).success).toBe(false));
+
+      it('succeeds with a roll of 11 vs 12', () => expect(navcheck({ navigator, DC: 12 }).success).toBe(true));
+
+      it('succeeds with a roll of 11 vs 7', () => expect(navcheck({ navigator, DC: 7 }).success).toBe(true));
+
+    });
+  });
+});
