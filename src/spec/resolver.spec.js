c4c8565761ecf44a53eaf458c9e3634608a5e15f
David Higgins <thehig@gmail.com>
2018-02-25 21:05:38 +0000

    Rename some vars, tests

--- src/spec/resolver.spec.js
+++ src/spec/resolver.spec.js
@@ -32,7 +32,7 @@ describe('resolver', () => {
       dice: { d20: _dArray([11]) },
     }).navigationCheck;
 
-    describe('with a bad navigator (-1)', () => {
+    describe('with modifier (-1)', () => {
       const navigator = { modifier: -1 };
 
       it('fails with a roll of 11 vs 15', () =>
@@ -48,7 +48,7 @@ describe('resolver', () => {
         expect(navcheck({ navigator, DC: 7 }).success).toBe(true));
     });
 
-    describe('with a good navigator (+1)', () => {
+    describe('with modifier (+1)', () => {
       const navigator = { modifier: +1 };
 
       it('fails with a roll of 11 vs 15', () =>
@@ -63,110 +63,119 @@ describe('resolver', () => {
       it('succeeds with a roll of 11 vs 7', () =>
         expect(navcheck({ navigator, DC: 7 }).success).toBe(true));
     });
+
+    describe('distance/pace', () => {
+      const navcheck = buildResolver({
+        paces: paceModifiers,
+        speeds,
+        dice: { d20: _dArray([11]) },
+      }).navigationCheck;
+      const navcheckD4Low = buildResolver({
+        paces: paceModifiers,
+        speeds,
+        dice: { d20: _dArray([11]), d4: _dArray([1, 2]) },
+      }).navigationCheck;
+      const navcheckD4High = buildResolver({
+        paces: paceModifiers,
+        speeds,
+        dice: { d20: _dArray([11]), d4: _dArray([3, 4]) },
+      }).navigationCheck;
+      const navigator = {};
+  
+      describe('on foot', () => {
+        it('should have distance 1 when pace is normal', () =>
+          expect(navcheck({ navigator }).distance).toBe(1));
+  
+        it('should have distance 0 when pace is slow and d4 rolls low', () =>
+          expect(navcheckD4Low({ navigator, pace: 'slow' }).distance).toBe(0));
+        it('should have distance 1 when pace is slow and d4 rolls high', () =>
+          expect(navcheckD4High({ navigator, pace: 'slow' }).distance).toBe(1));
+  
+        it('should have distance 1 when pace is fast and d4 rolls low', () =>
+          expect(navcheckD4Low({ navigator, pace: 'fast' }).distance).toBe(1));
+        it('should have distance 2 when pace is fast and d4 rolls high', () =>
+          expect(navcheckD4High({ navigator, pace: 'fast' }).distance).toBe(2));
+      });
+      describe('with a boat', () => {
+        it('should have distance 2 when pace is normal', () =>
+          expect(
+            navcheck({ navigator, speed: 'boat', pace: 'normal' }).distance
+          ).toBe(2));
+  
+        it('should have distance 1 when pace is slow and d4 rolls low', () =>
+          expect(
+            navcheckD4Low({ navigator, speed: 'boat', pace: 'slow' }).distance
+          ).toBe(1));
+        it('should have distance 2 when pace is slow and d4 rolls high', () =>
+          expect(
+            navcheckD4High({ navigator, speed: 'boat', pace: 'slow' }).distance
+          ).toBe(2));
+  
+        it('should have distance 2 when pace is fast and d4 rolls low', () =>
+          expect(
+            navcheckD4Low({ navigator, speed: 'boat', pace: 'fast' }).distance
+          ).toBe(2));
+        it('should have distance 3 when pace is fast and d4 rolls high', () =>
+          expect(
+            navcheckD4High({ navigator, speed: 'boat', pace: 'fast' }).distance
+          ).toBe(3));
+      });
+    });
+  
+    describe('lost & direction', () => {
+      const navcheckd6 = returnNumber => buildResolver({
+        paces: paceModifiers,
+        speeds,
+        dice: { d20: _dArray([11]), d6: _dArray(returnNumber) },
+      }).navigationCheck;
+  
+      it('should return direction N if fails with a 1', () => {
+        const check = navcheckd6([1])({ navigator, DC: 12 });
+        expect(check.lost).toBe(true);
+        expect(check.direction).toBe('N');
+      });
+  
+      it('should return direction NE if fails with a 2', () => {
+        const check = navcheckd6([2])({ navigator, DC: 12 });
+        expect(check.lost).toBe(true);
+        expect(check.direction).toBe('NE');
+      });
+  
+      it('should return direction SE if fails with a 3', () => {
+        const check = navcheckd6([3])({ navigator, DC: 12 });
+        expect(check.lost).toBe(true);
+        expect(check.direction).toBe('SE');
+      });
+  
+      it('should return direction S if fails with a 4', () => {
+        const check = navcheckd6([4])({ navigator, DC: 12 });
+        expect(check.lost).toBe(true);
+        expect(check.direction).toBe('S');
+      });
+  
+      it('should return direction SW if fails with a 5', () => {
+        const check = navcheckd6([5])({ navigator, DC: 12 });
+        expect(check.lost).toBe(true);
+        expect(check.direction).toBe('SW');
+      });
+  
+      it('should return direction NW if fails with a 6', () => {
+        const check = navcheckd6([6])({ navigator, DC: 12 });
+        expect(check.lost).toBe(true);
+        expect(check.direction).toBe('NW');
+      });
+    });
   });
 
-  describe('distance', () => {
-    const navcheck = buildResolver({
+
+
+  describe('encounter', () => {
+    const encounter = buildResolver({
       paces: paceModifiers,
       speeds,
       dice: { d20: _dArray([11]) },
-    }).navigationCheck;
-
-    const lonavcheck = buildResolver({
-      paces: paceModifiers,
-      speeds,
-      dice: { d20: _dArray([11]), d4: _dArray([1, 2]) },
-    }).navigationCheck;
-    const hinavcheck = buildResolver({
-      paces: paceModifiers,
-      speeds,
-      dice: { d20: _dArray([11]), d4: _dArray([3, 4]) },
-    }).navigationCheck;
-    const navigator = {};
-
-    describe('on foot', () => {
-      it('should have distance 1 when pace is normal', () =>
-        expect(navcheck({ navigator }).distance).toBe(1));
-
-      it('should have distance 0 when pace is slow and d4 rolls low', () =>
-        expect(lonavcheck({ navigator, pace: 'slow' }).distance).toBe(0));
-      it('should have distance 1 when pace is slow and d4 rolls high', () =>
-        expect(hinavcheck({ navigator, pace: 'slow' }).distance).toBe(1));
-
-      it('should have distance 1 when pace is fast and d4 rolls low', () =>
-        expect(lonavcheck({ navigator, pace: 'fast' }).distance).toBe(1));
-      it('should have distance 2 when pace is fast and d4 rolls high', () =>
-        expect(hinavcheck({ navigator, pace: 'fast' }).distance).toBe(2));
-    });
-    describe('with a boat', () => {
-      it('should have distance 2 when pace is normal', () =>
-        expect(
-          navcheck({ navigator, speed: 'boat', pace: 'normal' }).distance
-        ).toBe(2));
-
-      it('should have distance 1 when pace is slow and d4 rolls low', () =>
-        expect(
-          lonavcheck({ navigator, speed: 'boat', pace: 'slow' }).distance
-        ).toBe(1));
-      it('should have distance 2 when pace is slow and d4 rolls high', () =>
-        expect(
-          hinavcheck({ navigator, speed: 'boat', pace: 'slow' }).distance
-        ).toBe(2));
-
-      it('should have distance 2 when pace is fast and d4 rolls low', () =>
-        expect(
-          lonavcheck({ navigator, speed: 'boat', pace: 'fast' }).distance
-        ).toBe(2));
-      it('should have distance 3 when pace is fast and d4 rolls high', () =>
-        expect(
-          hinavcheck({ navigator, speed: 'boat', pace: 'fast' }).distance
-        ).toBe(3));
-    });
+    }).encounter;
   });
 
-  describe('direction', () => {
-    const navcheckd6 = returnNumber => buildResolver({
-      paces: paceModifiers,
-      speeds,
-      dice: { d20: _dArray([11]), d6: _dArray(returnNumber) },
-    }).navigationCheck;
-
-    it('should return direction N if fails with a 1', () => {
-      const check = navcheckd6([1])({ navigator, DC: 12 });
-      expect(check.lost).toBe(true);
-      expect(check.direction).toBe('N');
-    });
-
-    it('should return direction NE if fails with a 2', () => {
-      const check = navcheckd6([2])({ navigator, DC: 12 });
-      expect(check.lost).toBe(true);
-      expect(check.direction).toBe('NE');
-    });
-
-    it('should return direction SE if fails with a 3', () => {
-      const check = navcheckd6([3])({ navigator, DC: 12 });
-      expect(check.lost).toBe(true);
-      expect(check.direction).toBe('SE');
-    });
-
-    it('should return direction S if fails with a 4', () => {
-      const check = navcheckd6([4])({ navigator, DC: 12 });
-      expect(check.lost).toBe(true);
-      expect(check.direction).toBe('S');
-    });
-
-    it('should return direction SW if fails with a 5', () => {
-      const check = navcheckd6([5])({ navigator, DC: 12 });
-      expect(check.lost).toBe(true);
-      expect(check.direction).toBe('SW');
-    });
-
-    it('should return direction NW if fails with a 6', () => {
-      const check = navcheckd6([6])({ navigator, DC: 12 });
-      expect(check.lost).toBe(true);
-      expect(check.direction).toBe('NW');
-    });
-  });
-  describe('encounter', () => {});
   describe('weather', () => {});
 });
