e5e24df6754b4f6c5148fce195a2eb31702e4970
David Higgins <thehig@gmail.com>
2018-02-26 22:28:39 +0000

    Add thunk & redux devtools integration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(
+  /* Middlewares */
+  thunk
+));
+
 
 const rootReducer = combineReducers({
   form: formReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
