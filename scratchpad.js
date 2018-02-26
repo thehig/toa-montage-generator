5929d5814c66d4e826d252832e938d25a1166e7c
David Higgins <thehig@gmail.com>
2018-02-26 01:16:32 +0000

    Add useful example script to scratchpad

--- scratchpad.js
+++ scratchpad.js
@@ -1,33 +1,53 @@
-import { _dArray } from './src/logic/dice';
 import { resolver } from './src/logic/resolver';
 import { montage } from './src/logic/montage';
-
 import { paceModifiers, speeds, directions, weather } from './src/data/consts';
 
-// Take some override props and create a resolver with the default values and overrides
-const buildResolver = overrides =>
-  resolver(
-    Object.assign({}, { paces: paceModifiers, speeds, directions, weather }, overrides)
-  );
+const myMontage = montage(
+  resolver({ paces: paceModifiers, speeds, directions, weather })
+);
 
-const aGoodDaysResolver = buildResolver({
-  dice: {
-    d20: _dArray([
-      16, 12,     // Navigation with advantage
-      15, 4, 12,  // Encounters
-      3, 5, 2     // Weather
-    ])
-    // No d100 override because no Encounter triggers
-    // No d6 override because no Navigation fails
-    // No d4 override because pace is normal
-  }
-});
+const HewHackinstone = {
+  modifier: -5,
+  disadvantage: true,
+};
 
-const myMontage = montage(aGoodDaysResolver)({
-  navigator: {
-    advantage: true
-  }
-});
-const day = myMontage.day();
+const terrain = {
+  encounterDC: 20,
+  navigationDC: 20,
+};
 
-console.log(JSON.stringify(day, null, 4));
+const options = {
+  navigator: HewHackinstone,
+  pace: 'fast',
+  speed: 'boat',
+};
+
+const travelConfig = Object.assign({}, terrain, options);
+const numberOfDays = 4;
+
+const myResult = myMontage(travelConfig).travel(numberOfDays);
+
+if (myResult.completed) {
+  console.log(`Successfully travelled for ${numberOfDays} days`);
+}
+console.log(myResult.days.length, myResult.reasonsForStopping);
+
+const lastDay = myResult.days.pop();
+
+const myLastDayReadout = {
+  completed: myResult.completed /*?*/,
+  days: lastDay.index/*?*/,
+  'reason for stopping': myResult.reasonsForStopping/*?*/,
+  'nav rolls': lastDay.navigation.rolls.map(roll => roll.roll) /*?*/,
+  'encounter rolls': lastDay.encounters.map(
+    encounter => encounter.encounterRoll.roll
+  ) /*?*/,
+  encounters: lastDay.encounters
+    .filter(encounter => encounter.encounter !== false)
+    .map(encounter => encounter.encounter) /*?*/,
+  'weather rolls': lastDay.weather.map(
+    weather => weather.weatherRoll.roll
+  ) /*?*/,
+};
+
+console.log(myLastDayReadout);
