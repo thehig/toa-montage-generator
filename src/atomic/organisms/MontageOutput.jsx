b59d82be65ec61bbfa3bdb02686892e4c09224e3
David Higgins <thehig@gmail.com>
2018-03-03 03:33:02 +0000

    More output formatting

--- src/atomic/organisms/MontageOutput.jsx
+++ src/atomic/organisms/MontageOutput.jsx
@@ -1,35 +1,73 @@
 import React from 'react';
 
-const DiceRoll = ({ options, roll, rolls }) => <span>{options && options.name && `${options.name}: `}({roll})</span>;
-
-const MontageEncounter = ({ encounterRoll, encounter }) => (
+const DiceRoll = ({ options, roll, rolls }) => (
   <span>
-    <DiceRoll {...encounterRoll} /> {encounter && encounter}
+    { console.log(options, roll, )}
+    {options && options.name && `${options.name}: `}({rolls.join(', ')}){options && options.versus &&  ` v DC${options.versus}`}
   </span>
 );
 
+const MontageEncounter = ({ encounterRoll, encounter }) => (
+  <span>
+    <DiceRoll {...encounterRoll} /> {encounter && <strong>{` #${encounter}`}</strong>}
+  </span>
+);
+
+const MontageEncounters = ({ encounters }) => (
+  <div>
+    <h4>Encounters:</h4>
+    <ul>
+      {encounters.map((encounter, index) => (
+        <li key={index}>
+          <MontageEncounter {...encounter} />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 const MontageNavigation = ({
-  becameLost,
-  direction,
-  distance,
-  pace,
-  paceMod,
-  rolls,
-  speed,
-  startedLost,
-  success,
+  navigation: {
+    direction,
+    distance,
+    pace,
+    paceMod,
+    rolls,
+    speed,
+    startedLost,
+    lost,
+    becameLost,
+    becameFound,
+    success,
+  },
 }) => (
   <div>
     <h4>Navigation:</h4>
-    <div>Pace: {pace}</div>
-    <div>Speed: {speed}</div>
-    <div>Distance: {distance}</div>
-    <div>PaceMod: {paceMod}</div>
-    <div>Rolls: <ul>{rolls.map((roll, index) => <li key={index}><DiceRoll {...roll} /></li>)}</ul></div>
-    <div>Success: {success ? "true" : "false"}</div>
-    { startedLost && <div>STARTED LOST</div> }
-    { becameLost && <div>Became Lost</div> }
-    { becameLost && direction && <div>{direction}</div> }
+    <div>
+      <h4>Options:</h4>
+      <div>Pace: {pace}</div>
+      <div>PaceMod: {paceMod}</div>
+      <div>Speed: {speed}</div>
+    </div>
+    <div>
+      <h4>Outcome:</h4>
+      <div>Distance: {distance}</div>
+      <div>
+        Rolls:{' '}
+        <ul>
+          {rolls.map((roll, index) => (
+            <li key={index}>
+              <DiceRoll {...roll} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>Success: {success ? 'true' : 'false'}</div>
+      {lost && <div>LOST</div>}
+      {startedLost && <div>STARTED LOST</div>}
+      {becameLost && <div>Became Lost: {direction} </div>}
+      {becameFound && <div>Became Found</div>}
+    </div>
   </div>
 );
 
@@ -39,30 +77,25 @@ const MontageWeather = ({ weatherRoll, name, effect }) => (
   </span>
 );
 
+const MontageWeathers = ({ weathers }) => (
+  <div>
+    <h4>Weather:</h4>
+    <ul>
+      {weathers.map((weather, index) => (
+        <li key={index}>
+          <MontageWeather {...weather} />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 const MontageDay = ({ day: { index, encounters, navigation, weather } }) => (
   <div>
     <h3>Day {index}</h3>
-    <MontageNavigation {...navigation} />
-    <div>
-      <h4>Encounters:</h4>
-      <ul>
-        {encounters.map((encounter, index) => (
-          <li key={index}>
-            <MontageEncounter {...encounter} />
-          </li>
-        ))}
-      </ul>
-    </div>
-    <div>
-      <h4>Weather:</h4>
-      <ul>
-        {weather.map((w, index) => (
-          <li key={index}>
-            <MontageWeather {...w} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <MontageNavigation navigation={navigation} />
+    <MontageEncounters encounters={encounters} />
+    <MontageWeathers weathers={weather} />
   </div>
 );
 
@@ -95,14 +128,17 @@ const MontageOutput = props => {
   return (
     <div>
       <div>
-        <h4>Completed:</h4> {completed ? 'yes' : 'no'}
+        <h4>Montage Completed:</h4> {completed ? 'yes' : 'no'}
       </div>
       <div>
-        <h4>Distance:</h4> {distance} Hex(es)
+        <h4>Distance Travelled:</h4> {distance} Hex(es)
       </div>
-      <div>
-        <h4>Lost:</h4> {lost ? 'yes' : 'no'}
-      </div>
+      {lost && (
+        <div>
+          <h2>Currently Lost</h2>
+        </div>
+      )}
+
       <MontageReasons reasons={reasonsForStopping} />
       <MontageDays days={days} />
     </div>
