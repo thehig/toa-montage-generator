ed2ac8107dd3bc7b67e94775ea5a957bbecc74a2
David Higgins <thehig@gmail.com>
2018-03-03 20:04:27 +0000

    WIP on getting redux-form into tests

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import 'typeface-roboto';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import MUIButton from 'material-ui/Button';
 
 import { Field, reduxForm } from 'redux-form';
 import { action } from '@storybook/addon-actions';
@@ -12,12 +13,23 @@ export const MuiProvider = () => getStory => (
   <MuiThemeProvider theme={theme}>{getStory()}</MuiThemeProvider>
 );
 
+// export const reduxFormWrapper = formName => getStory =>
+//   reduxForm({
+//     form: formName,
+//   })(
 
-// Render story of component
-// Add redux form and state bindings
-export const reduxFormWrapper = formName => (getStory => {
-  const formWrapper = <form onSubmit={action('form onSubmit')}>{getStory()}</form>;
+//   );
+
+export const reduxFormWrapper = getStory => {
+  const FormWrapper = (
+    <form onSubmit={action('form onSubmit')}>
+      Story Here
+      <MUIButton type="submit">Submit</MUIButton>
+    </form>
+  );
+
+  // return FormWrapper;
   return reduxForm({
-    form: formName, // a unique identifier for this form
-  })(formWrapper);
-});
+    form: 'reduxFormWrapper'
+  })(FormWrapper);
+};
--- src/stories/index.js
+++ src/stories/index.js
@@ -6,16 +6,24 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Field, reduxForm } from 'redux-form';
 
+import { MenuItem } from 'material-ui/Menu';
+
 import { SelectField, TextField, Checkbox, RadioGroup } from '../atomic';
 import { reduxFormWrapper } from '../../.storybook/decorators';
 
 // 1. Atoms
 const atoms = storiesOf('1.Atoms', module);
-atoms.addDecorator(reduxFormWrapper('atoms'));
-atoms.add('SelectField', () => <Field name="SelectField" label="SelectField" component={SelectField} />);
-atoms.add('TextField', () => <Field name="TextField" label="TextField" component={TextField} />);
-atoms.add('Checkbox', () => <Field name="Checkbox" label="Checkbox" component={Checkbox} />);
-atoms.add('RadioGroup', () => <Field name="RadioGroup" label="RadioGroup" component={RadioGroup} />);
+atoms.addDecorator(reduxFormWrapper);
+atoms.add('SelectField', () => (
+  <Field name="selectField" component={SelectField} label="SelectField">
+    <MenuItem value="slow">Slow</MenuItem>
+    <MenuItem value="normal">Normal</MenuItem>
+    <MenuItem value="fast">Fast</MenuItem>
+  </Field>
+));
+// atoms.add('TextField', () => <Field name="TextField" label="TextField" component={TextField} />);
+// atoms.add('Checkbox', () => <Field name="Checkbox" label="Checkbox" component={Checkbox} />);
+// atoms.add('RadioGroup', () => <Field name="RadioGroup" label="RadioGroup" component={RadioGroup} />);
 
 // 3. Organisms
 // MontageForm
@@ -23,4 +31,3 @@ atoms.add('RadioGroup', () => <Field name="RadioGroup" label="RadioGroup" compon
 
 // 4. Ecosystems
 // MontagePage
-
