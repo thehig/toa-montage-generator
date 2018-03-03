027ab812934c93f747432ce29f0d1cd198166bac
David Higgins <thehig@gmail.com>
2018-03-03 19:45:45 +0000

    Create and use decorator to create simplified forms for testing

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -3,14 +3,21 @@ import React from 'react';
 import 'typeface-roboto';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
+import { Field, reduxForm } from 'redux-form';
+import { action } from '@storybook/addon-actions';
+
 const theme = createMuiTheme();
 
-// const MuiProvider = function () {
-//   return function (getStory) {
-//     return <MuiThemeProvider theme={theme}>{getStory()}</MuiThemeProvider>;
-//   }
-// }
+export const MuiProvider = () => getStory => (
+  <MuiThemeProvider theme={theme}>{getStory()}</MuiThemeProvider>
+);
 
-// export const MuiProvider = () => getStory => (
-//   <MuiThemeProvider theme={theme}>{getStory()}</MuiThemeProvider>
-// );
+
+// Render story of component
+// Add redux form and state bindings
+export const reduxFormWrapper = formName => (getStory => {
+  const formWrapper = <form onSubmit={action('form onSubmit')}>{getStory()}</form>;
+  return reduxForm({
+    form: formName, // a unique identifier for this form
+  })(formWrapper);
+});
--- src/stories/index.js
+++ src/stories/index.js
@@ -4,11 +4,23 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import { Field, reduxForm } from 'redux-form';
+
 import { SelectField, TextField, Checkbox, RadioGroup } from '../atomic';
+import { reduxFormWrapper } from '../../.storybook/decorators';
 
+// 1. Atoms
 const atoms = storiesOf('1.Atoms', module);
+atoms.addDecorator(reduxFormWrapper('atoms'));
+atoms.add('SelectField', () => <Field name="SelectField" label="SelectField" component={SelectField} />);
+atoms.add('TextField', () => <Field name="TextField" label="TextField" component={TextField} />);
+atoms.add('Checkbox', () => <Field name="Checkbox" label="Checkbox" component={Checkbox} />);
+atoms.add('RadioGroup', () => <Field name="RadioGroup" label="RadioGroup" component={RadioGroup} />);
 
+// 3. Organisms
+// MontageForm
+// MontageOutput
 
+// 4. Ecosystems
+// MontagePage
 
-// const organisms = storiesOf('3.Organisms', module);
-// const ecosystems = storiesOf('4.Ecosystems', module);
