62e1a933df87728360a69ff78371faa5036016bc
David Higgins <thehig@gmail.com>
2018-03-03 00:58:10 +0000

    Delete previous attempts at designing UI before code

--- src/atomic/index.js
+++ src/atomic/index.js
@@ -1,10 +1,2 @@
-export { default as OrgDuration } from './organisms/Duration';
-export { default as OrgNavigation } from './organisms/Navigation';
-export { default as OrgTerrain } from './organisms/Terrain';
-export { default as OrgPace } from './organisms/Pace';
-
-export { default as EcoList } from './ecosystems/List';
-
-
 export * from './atoms/MUI-Wrappers';
 
--- src/atomic/organisms/MontageForm.jsx
+++
@@ -1,8 +0,0 @@
-import { reduxForm } from 'redux-form';
-import MontageFormMolecule from '../molecules/MontageForm';
-
-const MontageForm = reduxForm({
-  form: 'montage'
-})(MontageFormMolecule);
-
-export default MontageForm;
