dc314206e7668edd0023d2cae9d77f8a833ed407
David Higgins <thehig@gmail.com>
2018-03-03 19:31:00 +0000

    Remove old stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,5 +4,11 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-const organisms = storiesOf('3.Organisms', module);
-const ecosystems = storiesOf('4.Ecosystems', module);
+import { SelectField, TextField, Checkbox, RadioGroup } from '../atomic';
+
+const atoms = storiesOf('1.Atoms', module);
+
+
+
+// const organisms = storiesOf('3.Organisms', module);
+// const ecosystems = storiesOf('4.Ecosystems', module);
