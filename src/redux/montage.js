a6b4125394c0cfce57a8084d3d49aa21f7e62aa8
David Higgins <thehig@gmail.com>
2018-03-03 02:20:25 +0000

    Add montage wrapper runner

--- src/redux/montage.js
+++ src/redux/montage.js
@@ -1,3 +1,5 @@
+import { montage } from '../logic/wrapper';
+
 export const CONSTS = {
   MONTAGE_SUBMIT: "MONTAGE_SUBMIT"
 };
@@ -24,15 +26,38 @@ export const montageSubmit = (montage) => ({
   payload: montage
 });
 
+const runMontage = options => montage({
+  navigator: {
+    modifier: options.modifier,
+    advantage: options.advantage,
+    disadvantage: options.disadvantage,
+  },
+  pace: options.pace,
+  speed: options.speed,
+  encounterDC: options.encounterDC,
+  navigationDC: options.navigationDC,
+}).travel(options.numdays, {
+  daysOffset: options.daysoffset,
+  lost: options.lost,
+});
+
+
 export const reducer = (state = initialState.montage, action) => {
   switch (action.type) {
-    case CONSTS.MONTAGE_SUBMIT: return {
-      ...state,
-      options: {
+    case CONSTS.MONTAGE_SUBMIT: {
+      const options = {
         ...state.options,
         ...action.payload
-      }
-    };
+      };
+
+      const content = runMontage(options);
+
+      return {
+        ...state,
+        options,
+        content
+      };
+    }
     default: return state;
   }
 };
