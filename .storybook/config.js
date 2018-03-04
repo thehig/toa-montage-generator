45ba1d1d294c9a94ff6fdbe26dd92d1f42ecaa0f
David Higgins <thehig@gmail.com>
2018-03-04 00:15:15 +0000

    Update config for new stories file name

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { MuiProvider } from './decorators';
 
 function loadStories() {
-  require('../src/stories');
+  require('../src/stories/field-components');
 }
 
 configure(loadStories, module);
