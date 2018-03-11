1c5d35b98b8a284bb88f5737efedfdea2e18d04a
David Higgins <thehig@gmail.com>
2018-03-11 14:00:11 +0000

    Create new redux form decorator that provides store, theme, and rf'd form

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -41,13 +41,11 @@ export const ThemeDecorator = theme => getStory => (
   </MuiThemeProvider>
 );
 
-const FormWrapperBase = ({ children = null, handleSubmit }) => (
+export const FormWrapperBase = ({ children = null, handleSubmit }) => (
   // Note that even though the prop here is called 'handleSubmit',
   //  we should pass the prop named 'onSubmit' for redux form to work its magic
   // EG: <FormWrapper onSubmit={action('handleSubmit')}>
-  <form onSubmit={handleSubmit}>
-    {children}
-  </form>
+  <form onSubmit={handleSubmit}>{children}</form>
 );
 /**
  * Create a <form> component bound with reduxForm and an onSubmit function
@@ -56,15 +54,34 @@ export const FormWrapper = reduxForm({
   form: 'reduxFormWrapper',
 })(FormWrapperBase);
 
+/**
+ * 
+ * @param {object} options
+ * @param {object} options.initialState Initial redux state
+ * @param {object} options.theme Material-UI theme object intended for createMuiTheme()
+ */
+export const ReduxFormDecorator = ({
+  initialState = {},
+  theme = {},
+  ...formOptions
+} = {}) => getStory => (
+  <Provider store={mockFormStore({ ...defaultInitialState, ...initialState })}>
+    <MuiThemeProvider theme={createMuiTheme({ ...defaultTheme, ...theme })}>
+      <FormWrapper {...formOptions}>{getStory()}</FormWrapper>
+    </MuiThemeProvider>
+  </Provider>
+);
 
 /**
  * Take two sets of props to be applied to a Redux Form Field component
- * Returns 
+ * Returns
  *    <FormWrapper> - a reduxForm()'ed, <form> component with submit bound
  *    <Field> - a redux-form component with props spread
  *    <Button> - a button with type="submit" which will trigger the FormWrapper onSubmit
  */
-export const ReduxFormWithSingleField = (higherPriorityProps = {}) => ( lowerPriorityProps = {}) => (
+export const ReduxFormWithSingleField = (higherPriorityProps = {}) => (
+  lowerPriorityProps = {}
+) => (
   <FormWrapper onSubmit={action(`${name} handleSubmit`)}>
     <Field {...lowerPriorityProps} {...higherPriorityProps} />
     <MUIButton color="primary" type="submit">
--- src/atomic/molecules/stories.js
+++ src/atomic/molecules/stories.js
@@ -7,33 +7,30 @@ import { storiesOf } from '@storybook/react';
 import blue from 'material-ui/colors/blue';
 
 // Storybook Decorators
-import { ReduxDecorator, ThemeDecorator, FormWrapper } from '../../../.storybook/decorators';
+import {
+  ReduxFormDecorator
+} from '../../../.storybook/decorators';
 
 // Components
-import {
-  DCInput, Navigator, PaceAndSpeed, Travel
-} from '../';
+import { DCInput, Navigator, PaceAndSpeed, Travel } from '../';
 import { action } from '@storybook/addon-actions';
+
+const decorator = ReduxFormDecorator({
+  initialState: {
+    _init: "Initialized by Molecules stories"
+  },
+  theme: {
+    palette: {
+      secondary: blue,
+    },
+  },
+  onSubmit: action('On Molecule Submit')
+});
 
 // eslint-disable-next-line
 storiesOf(`Molecules`, module)
   // Add redux <Provider> with store
-  .addDecorator(
-    ReduxDecorator({
-      /* Initial redux state */
-      stories: `molecules`,
-    })
-  )
-  // Add <MuiThemeProvider> with theme
-  .addDecorator(
-    ThemeDecorator({
-      /* Base theme overrides */
-      palette: {
-        primary: blue,
-      },
-    })
-  )
-  // .addDecorator( FormWrapper() )
+  .addDecorator(decorator)
   .add('DCInput', () => <DCInput />)
   .add('Navigator', () => <Navigator />)
   .add('PaceAndSpeed', () => <PaceAndSpeed />)
