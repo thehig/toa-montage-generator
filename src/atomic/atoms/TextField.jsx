ef1a1d60b7ba13411b0bdfb8e17b5e415fd938ce
David Higgins <thehig@gmail.com>
2018-03-11 14:35:46 +0000

    Add paddingBottom to TextField

--- src/atomic/atoms/TextField.jsx
+++ src/atomic/atoms/TextField.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { withStyles } from 'material-ui/styles';
 import {
   FormControl,
   FormHelperText,
@@ -9,13 +10,20 @@ import {
 // import { InputLabel as MUIInputLabel } from 'material-ui/Input';
 import MUITextField from 'material-ui/TextField';
 
+const styles = theme => ({
+  root: {
+    paddingBottom: theme.spacing.unit
+  }
+});
+
 export const TextField = ({
   input,
   label,
   meta: { touched, error },
+  classes,
   ...custom
 }) => (
-  <FormControl error={Boolean(touched && error)} fullWidth>
+  <FormControl error={Boolean(touched && error)} fullWidth className={classes.root}>
     <MUITextField
       error={Boolean(touched && error)}
       label={label}
@@ -26,4 +34,4 @@ export const TextField = ({
   </FormControl>
 );
 
-export default TextField;
+export default withStyles(styles)(TextField);
