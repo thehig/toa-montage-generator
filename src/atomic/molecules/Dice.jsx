5eb17deacc52dd8b32c1b63ff83286a3fc248636
David Higgins <thehig@gmail.com>
2018-03-14 20:31:56 +0000

    Add layout for Dice (wip)

--- src/atomic/molecules/Dice.jsx
+++ src/atomic/molecules/Dice.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+
 import { withStyles } from 'material-ui/styles';
 
 import MUIgreen from 'material-ui/colors/green';
@@ -12,12 +15,18 @@ import { D20 } from '../';
 
 const styles = theme => ({
   root: {
-    width: "100%",
+    width: "30%",
     height: theme.spacing.unit * 12,
   },
   dice: {
     height: "100%"
   },
+  container: {
+    height: "100%"
+  },
+  gridItem: {
+    height: "100%"
+  }
 });
 
 const Dice = ({
@@ -29,12 +38,23 @@ const Dice = ({
   ...diceProps
 } = {}) => (
   <Paper className={classes.root}>
-    <D20
-      className={classes.dice}
-      value={roll}
-      color={success === true ? green : red}
-      {...diceProps}
-    />
+    <Grid container className={classes.container}>
+      <Grid item xs={3} className={classes.gridItem}>
+        <D20
+          className={classes.dice}
+          value={roll}
+          color={success === true ? green : red}
+          {...diceProps}
+        />
+      </Grid>
+      <Grid item xs={3} className={classes.gridItem}>
+        { name && <Typography variant="body1">{name}</Typography> }
+        { modifier && <Typography variant="body1">Modifier {`${modifier > 0 && "+"}${modifier}`}</Typography> }
+        { versus && <Typography variant="body1">Versus {versus}</Typography> }
+        { advantage && <Typography variant="body1">Advantage</Typography> }
+        { disadvantage && <Typography variant="body1">Disadvantage</Typography> }
+      </Grid>
+    </Grid>
   </Paper>
 );
 
