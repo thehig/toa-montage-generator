95b6c355ebdcb5c37f35fdcc0c03f55f1ed6a3dc
David Higgins <thehig@gmail.com>
2018-03-17 17:50:46 +0000

    Add stories of the new components

--- src/atomic/molecules/stories.js
+++ src/atomic/molecules/stories.js
@@ -8,7 +8,7 @@ import { withKnobs, number, color } from '@storybook/addon-knobs/react';
 import { ThemeDecorator } from '../../../.storybook/decorators';
 
 // Components
-import { Dice } from '../';
+import { Dice, Day, Encounters, Navigation, Weathers } from '../';
 
 // eslint-disable-next-line
 const stories = storiesOf(`Molecules`, module)
@@ -51,7 +51,165 @@ const stories = storiesOf(`Molecules`, module)
     roll: 9,
   },
 ].map((roll, index) => {
-  stories.add(`Example (${index}): ${roll.options.name}`, () => (
+  stories.add(`Dice (${index}): ${roll.options.name}`, () => (
     <Dice {...roll} />
   ));
 });
+
+stories.add('Encounters', () => (<Encounters encounters={montage.days[0].encounters} />));
+stories.add('Weathers', () => (<Weathers weathers={montage.days[0].weathers}/>));
+
+stories.add('Navigation', () => (<Navigation encounters={montage.days[0].encounters}/>));
+
+stories.add('Day', () => (<Day day={montage.days[0]}/>));
+
+const montage = {
+  "days": [
+    {
+      "navigation": {
+        "rolls": [
+          {
+            "options": {
+              "advantage": false,
+              "disadvantage": false,
+              "name": "navigation check",
+              "versus": 10
+            },
+            "rolls": [
+              15
+            ],
+            "roll": 15,
+            "success": true
+          }
+        ],
+        "startedLost": false,
+        "pace": "normal",
+        "speed": "walk",
+        "paceMod": 0,
+        "success": true,
+        "lost": false,
+        "distance": 1
+      },
+      "encounters": [
+        {
+          "encounterRoll": {
+            "options": {
+              "versus": 10,
+              "name": "Encounter Chance"
+            },
+            "rolls": [
+              20
+            ],
+            "roll": 20,
+            "success": true
+          },
+          "tableRoll": {
+            "options": {
+              "name": "Encounter Table"
+            },
+            "rolls": [
+              40
+            ],
+            "roll": 40
+          },
+          "encounter": 40
+        },
+        {
+          "encounterRoll": {
+            "options": {
+              "versus": 10,
+              "name": "Encounter Chance"
+            },
+            "rolls": [
+              1
+            ],
+            "roll": 1,
+            "success": false
+          },
+          "encounter": false
+        },
+        {
+          "encounterRoll": {
+            "options": {
+              "versus": 10,
+              "name": "Encounter Chance"
+            },
+            "rolls": [
+              16
+            ],
+            "roll": 16,
+            "success": true
+          },
+          "tableRoll": {
+            "options": {
+              "name": "Encounter Table"
+            },
+            "rolls": [
+              19
+            ],
+            "roll": 19
+          },
+          "encounter": 19
+        }
+      ],
+      "weather": [
+        {
+          "weatherRoll": {
+            "options": "Weather",
+            "rolls": [
+              20
+            ],
+            "roll": 20
+          },
+          "name": "torrent",
+          "effect": {
+            "min": 19,
+            "max": 20,
+            "name": "Torrent",
+            "effects": ""
+          }
+        },
+        {
+          "weatherRoll": {
+            "options": "Weather",
+            "rolls": [
+              17
+            ],
+            "roll": 17
+          },
+          "name": "heavy",
+          "effect": {
+            "min": 16,
+            "max": 18,
+            "name": "Heavy",
+            "effects": ""
+          }
+        },
+        {
+          "weatherRoll": {
+            "options": "Weather",
+            "rolls": [
+              18
+            ],
+            "roll": 18
+          },
+          "name": "heavy",
+          "effect": {
+            "min": 16,
+            "max": 18,
+            "name": "Heavy",
+            "effects": ""
+          }
+        }
+      ],
+      "index": 1
+    }
+  ],
+  "completed": false,
+  "reasonsForStopping": [
+    "Encounter(s)",
+    "Weather"
+  ],
+  "distance": 1,
+  "lost": false
+};
