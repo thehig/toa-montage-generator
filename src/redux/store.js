0254946df3e58a16eb98f757805b89707277299f
David Higgins <thehig@gmail.com>
2018-03-18 15:25:14 +0000

    Add saga boilerplate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,33 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import { reducer as montage } from './montage';
+import sagas from './sagas';
 
+// Hook redux devtools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(applyMiddleware(
-  /* Middlewares */
-  thunk
-));
 
+// Create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
 
+// Create root reducer
 const rootReducer = combineReducers({
   form: formReducer,
   montage
 });
 
+// Load middlewares
+const enhancer = composeEnhancers(applyMiddleware(
+  thunk,
+  sagaMiddleware
+));
+
+// Create store with reducer and middleware
 const store = createStore(rootReducer, enhancer);
+
+// Run sagas
+sagaMiddleware.run(sagas);
 
 export default store;
