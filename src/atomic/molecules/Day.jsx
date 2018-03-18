63a90b07e34ff874598b24b0059e472e4147f8c7
David Higgins <thehig@gmail.com>
2018-03-18 17:32:07 +0000

    Add summary to day

--- src/atomic/molecules/Day.jsx
+++ src/atomic/molecules/Day.jsx
@@ -13,31 +13,44 @@ import { Navigation, Encounters, Weathers } from '../';
 
 class Day extends React.Component {
   state = {
-    expanded: false
+    expanded: false,
   };
 
   handleClick = () => {
     this.setState({ expanded: !this.state.expanded });
   };
 
+  createSummary = () => {
+    const { day } = this.props;
+    const lost = day.navigation.lost && day.navigation.direction;
+    const encounter = day.encounters
+      .filter(e => e.encounter)
+      .map(e => e.tableRoll.roll);
+    const weather = day.weather.filter(w => w.name === 'torrent').length > 0;
+
+    const summary = [
+      lost && `Lost (${lost})`,
+      encounter.length && `Encounter(s) [${encounter.join(', ')}]`,
+      weather && 'Weather',
+    ]
+      .filter(e => e)
+      .join(', ');
+
+    return summary;
+  };
+
   render() {
     const { classes, day } = this.props;
+
     const { expanded } = this.state;
 
     return [
-      <ListItem
-        key={`${day.index}-day`}
-        button
-        onClick={this.handleClick}>
+      <ListItem key={`${day.index}-day`} button onClick={this.handleClick}>
         <ListItemIcon>
           <TodayIcon />
         </ListItemIcon>
-        <ListItemText inset primary={`Day ${day.index}`} />
-        {expanded ? (
-          <ExpandLess />
-        ) : (
-          <ExpandMore />
-        )}
+        <ListItemText inset primary={`Day ${day.index}`} secondary={this.createSummary()} />
+        {expanded ? <ExpandLess /> : <ExpandMore />}
       </ListItem>,
       <Collapse
         key={`${day.index}-day-list`}
