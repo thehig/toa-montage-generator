adf149ca4a65aaf29a3a38ddcd6ca496c373499c
David Higgins <thehig@gmail.com>
2018-03-18 21:45:10 +0000

    Remove almost everything from TerrainForm

--- src/atomic/organisms/TerrainForm.jsx
+++ src/atomic/organisms/TerrainForm.jsx
@@ -11,94 +11,31 @@ import PlayArrowIcon from 'material-ui-icons/PlayArrow';
 import RefreshIcon from 'material-ui-icons/Refresh';
 import { TextField, SelectField, CheckboxGroup } from '../';
 
+import { terrain } from '../../logic/consts';
+
 const styles = theme => ({});
 
-const MontageForm = props => {
+const TerrainForm = props => {
   const { handleSubmit, pristine, reset, submitting, classes } = props;
   return (
     <form onSubmit={handleSubmit}>
       <MUICard className={classes.card}>
         <CardContent>
           <MUITypography className={classes.title}>
-            Montage settings
+            Terrain
           </MUITypography>
+          
           <Field
-            name="numdays"
-            key="numdays"
-            component={TextField}
-            label="Number of days to travel for"
-            type="number"
-          />
-          <Field
-            name="encounterDC"
-            key="encounterDC"
-            component={TextField}
-            label="Encounter DC (higher is less likely)"
-            type="number"
-          />
-          <Field
-            name="speed"
-            key="speed"
+            name="terrain"
+            key="terrain"
             component={SelectField}
-            label="Speed (affects hexes travelled)">
-            <MenuItem value="walk">Walking</MenuItem>
-            <MenuItem value="boat">Boating</MenuItem>
+            label="Terrain">
+            { terrain.map(t => <MenuItem key={`terrain-${t.id}`} value={t.id}>{t.name}</MenuItem>) }
           </Field>
-          <Field
-            name="starts-lost"
-            key="starts-lost"
-            component={CheckboxGroup}
-            options={[{ value: 'lost', label: 'Montage starts Lost' }]}
-          />
-          <Field
-            name="daysoffset"
-            key="daysoffset"
-            component={TextField}
-            label="Offset for number of days past"
-            type="number"
-          />
+          
         </CardContent>
       </MUICard>
-      <MUICard className={classes.card}>
-        <CardContent>
-          <MUITypography className={classes.title}>
-            Navigation Check
-          </MUITypography>
-          <Field
-            name="navigationDC"
-            key="navigationDC"
-            component={TextField}
-            label="Navigation DC"
-            type="number"
-          />
-          <Field
-            name="pace"
-            key="pace"
-            component={SelectField}
-            label="Pace (affects navigation DC & hexes travelled) ">
-            <MenuItem value="slow">Slow</MenuItem>
-            <MenuItem value="normal">Normal</MenuItem>
-            <MenuItem value="fast">Fast</MenuItem>
-          </Field>
-          <Field
-            name="modifier"
-            key="nav-modifier"
-            component={TextField}
-            label="Navigators total modifier"
-            type="number"
-          />
-          <Field
-            name="nav-advantage"
-            key="nav-advantage"
-            component={CheckboxGroup}
-            options={[
-              { value: 'advantage', label: 'Advantage' },
-              { value: 'disadvantage', label: 'Disadvantage' },
-            ]}
-          />
-        </CardContent>
-      </MUICard>
-
+      
       <MUIButton
         aria-label="reset"
         disabled={submitting}
@@ -120,34 +57,34 @@ const MontageForm = props => {
   );
 };
 
-const validate = values => {
-  const errors = {};
-  const requiredFields = [
-    'modifier',
-    'pace',
-    'speed',
-    'navigationDC',
-    'encounterDC',
-    'numdays',
-  ];
-  requiredFields.forEach(field => {
-    if (!values[field]) {
-      errors[field] = 'Required';
-    }
-  });
-  return errors;
-};
+// const validate = values => {
+//   const errors = {};
+//   const requiredFields = [
+//     'modifier',
+//     'pace',
+//     'speed',
+//     'navigationDC',
+//     'encounterDC',
+//     'numdays',
+//   ];
+//   requiredFields.forEach(field => {
+//     if (!values[field]) {
+//       errors[field] = 'Required';
+//     }
+//   });
+//   return errors;
+// };
 
 export default reduxForm({
-  form: 'MontageForm', // a unique identifier for this form
+  form: 'TerrainForm', // a unique identifier for this form
   initialValues: {
-    modifier: 3,
-    pace: 'normal',
-    speed: 'walk',
-    navigationDC: 15,
-    encounterDC: 19,
-    numdays: 10,
-    "nav-advantage": ""
+    // modifier: 3,
+    // pace: 'normal',
+    // speed: 'walk',
+    // navigationDC: 15,
+    // encounterDC: 19,
+    // numdays: 10,
+    // "nav-advantage": ""
   },
-  validate,
-})(withStyles(styles)(MontageForm));
+  // validate,
+})(withStyles(styles)(TerrainForm));
